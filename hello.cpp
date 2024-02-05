5d8e9adf4bf173284899f5bff1763189a2555cf7
chao hong <chaohong.mg12@nycu.edu.tw>
2024-02-05 20:20:51 +0800

    add two numbers

--- hello.cpp
+++ hello.cpp
@@ -2,9 +2,14 @@
 # include <string>
 using namespace std;
 
+int add(int a, int b) {
+    return a + b ;
+}
+
 int main() {
     string input ;
     getline(cin, input) ;
-    cout << "hello " << input ;
+    cout << "hello " << input << endl ;
+    cout << add(5, 6) ;
     return 0 ;
 }
