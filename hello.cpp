c6cf442013be70662cc62c3dab4b2e5fd7af2cfe
chao hong <chaohong.mg12@nycu.edu.tw>
2024-02-05 20:26:35 +0800

    subtract two integers

--- hello.cpp
+++ hello.cpp
@@ -2,9 +2,14 @@
 # include <string>
 using namespace std;
 
+int subtract(int a, int b) {
+    return a - b ;
+}
+
 int main() {
     string input ;
     getline(cin, input) ;
     cout << "hello " << input ;
+    cout << subtract(5, 6) ;
     return 0 ;
 }
